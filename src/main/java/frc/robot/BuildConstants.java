c800c8c6ca32a0eb12eea9d6f407364e9be51e57
rishabhreng <rishabhreng@gmail.com>
2025-01-18 17:01:37 -0600

    advkit initial drive template

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2025-Dragoneel";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "44ef4ca77fe8cd0e238d4d19c3554c55d6f12d1a";
-  public static final String GIT_DATE = "2025-01-16 18:59:10 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2025-01-16 20:55:33 EST";
-  public static final long BUILD_UNIX_TIME = 1737078933224L;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "0b0fb55c27f828a99a0e57ee31412c24ff94eaa1";
+  public static final String GIT_DATE = "2025-01-16 21:00:14 EST";
+  public static final String GIT_BRANCH = "AdvKit-Drive";
+  public static final String BUILD_DATE = "2025-01-18 18:00:39 EST";
+  public static final long BUILD_UNIX_TIME = 1737241239894L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
