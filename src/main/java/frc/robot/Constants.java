346145bd55e7232f646e109d8bec39c52c76297b
rishabhreng <rishabhreng@gmail.com>
2025-01-30 20:57:31 -0600

    manual fix merge conflicts

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,6 +13,22 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Rotations;
+import static edu.wpi.first.units.Units.Volts;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.GravityTypeValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.RobotController;
 
@@ -34,5 +50,101 @@ public final class Constants {
 
     /** Replaying from a log file. */
     REPLAY
+  }
+
+  public static class ScorerConstants {
+    /* Scorer motor ID */
+    public static final int SCORER_MOTOR_ID = 0;
+
+    public static final SparkFlexConfig SCORER_CONFIG =
+        (SparkFlexConfig)
+            new SparkFlexConfig()
+                .inverted(false)
+                .idleMode(IdleMode.kCoast)
+                .voltageCompensation(RobotController.getBatteryVoltage())
+                .smartCurrentLimit(20);
+
+    /* Scorer motor state */
+    public static enum ScorerState {
+      IDLE(Volts.of(0.0)),
+      INTAKE_CORAL(Volts.of(6.0)),
+      SHOOT_CORAL(Volts.of(-6.0)),
+      INTAKE_ALGAE(Volts.of(-6.0));
+
+      public final Voltage volts;
+
+      private ScorerState(Voltage volts) {
+        this.volts = volts;
+      }
+    }
+  }
+
+  public static class ClimbConstants {
+
+    public static final int CLIMB_MOTOR_ID = 16;
+    public static final Voltage CLIMB_RUN_VOLTS = Volts.of(9.0);
+  }
+
+  public static class ElevatorConstants {
+
+    public static enum ElevatorState {
+      STOW(Rotations.of(0.0)),
+      L1(Rotations.of(3.0)),
+      L2(Rotations.of(6.0)),
+      STATION(Rotations.of(7.5)),
+      L3(Rotations.of(9.0)),
+      L4(Rotations.of(12.0)),
+      NET(Rotations.of(15.0));
+
+      public final Angle height;
+
+      private ElevatorState(Angle height) {
+        this.height = height;
+      }
+    }
+
+    public static final int mainElevatorMotorID = 13;
+    public static final int followerElevatorMotorID = 14;
+
+    public static final double elevatorGearRatio = 6.0;
+    public static final double elevatorShaftRadiusInches = 0.5;
+
+    public static final TalonFXConfiguration elevatorConfig =
+        new TalonFXConfiguration()
+            .withMotorOutput(
+                new MotorOutputConfigs()
+                    .withInverted(InvertedValue.CounterClockwise_Positive)
+                    .withNeutralMode(NeutralModeValue.Brake))
+            .withCurrentLimits(
+                new CurrentLimitsConfigs()
+                    .withStatorCurrentLimitEnable(true)
+                    .withStatorCurrentLimit(80)
+                    .withSupplyCurrentLimitEnable(true)
+                    .withSupplyCurrentLimit(70)
+                    .withSupplyCurrentLowerLimit(40)
+                    .withSupplyCurrentLowerTime(1.0))
+            .withSlot0(
+                new Slot0Configs()
+                    .withGravityType(GravityTypeValue.Elevator_Static)
+                    // TODO: these need to be tuned
+                    .withKP(0)
+                    .withKI(0)
+                    .withKD(0)
+                    .withKS(0)
+                    .withKV(0)
+                    .withKA(0))
+            .withMotionMagic(
+                new MotionMagicConfigs()
+                    // TODO: these need to change
+                    .withMotionMagicCruiseVelocity(0)
+                    .withMotionMagicAcceleration(0)
+                    .withMotionMagicJerk(0))
+            .withSoftwareLimitSwitch(
+                new SoftwareLimitSwitchConfigs()
+                    // TODO: these may need to change
+                    .withForwardSoftLimitEnable(true)
+                    .withForwardSoftLimitThreshold(100)
+                    .withReverseSoftLimitEnable(true)
+                    .withReverseSoftLimitThreshold(0));
   }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,16 +13,13 @@
 
 package frc.robot;
 
+import static frc.robot.subsystems.vision.VisionConstants.*;
+
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.subsystems.elevator.Elevator;
-import frc.robot.subsystems.elevator.ElevatorIO;
-import frc.robot.subsystems.elevator.ElevatorIOSim;
-import frc.robot.subsystems.elevator.ElevatorIOTalonFX;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.climb.Climb;
@@ -33,13 +30,16 @@ import frc.robot.subsystems.drive.gyro.GyroIOPigeon2;
 import frc.robot.subsystems.drive.module.ModuleIO;
 import frc.robot.subsystems.drive.module.ModuleIOSim;
 import frc.robot.subsystems.drive.module.ModuleIOTalonFX;
+import frc.robot.subsystems.elevator.Elevator;
+import frc.robot.subsystems.elevator.ElevatorIO;
+import frc.robot.subsystems.elevator.ElevatorIOSim;
+import frc.robot.subsystems.elevator.ElevatorIOTalonFX;
 import frc.robot.subsystems.scorer.Scorer;
 import frc.robot.subsystems.scorer.ScorerIO;
 import frc.robot.subsystems.scorer.ScorerIOSim;
 import frc.robot.subsystems.vision.Vision;
 import frc.robot.subsystems.vision.VisionIO;
 import frc.robot.subsystems.vision.VisionIOPhotonVisionSim;
-import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
