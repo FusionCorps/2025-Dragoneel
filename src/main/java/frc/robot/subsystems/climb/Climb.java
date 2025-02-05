0d4bc9000333d0512733bd12962ab86a728dbcd5
6672DriveLaptop1 <rishabhreng@gmail.com>
2025-02-04 18:57:26 -0600

    subsystems: (Drive) drives straight
    had to rezero encoder offsets

--- src/main/java/frc/robot/subsystems/climb/Climb.java
+++ src/main/java/frc/robot/subsystems/climb/Climb.java
@@ -35,7 +35,8 @@ public class Climb extends SubsystemBase {
 
   /* Run the climb motor and hold position in brake mode when stopped. */
   public Command runClimbCommand() {
-    // Note that the end runnable sets the voltage to 0V, but the neutral mode configuration is set to brake mode.
+    // Note that the end runnable sets the voltage to 0V, but the neutral mode configuration is set
+    // to brake mode.
     return runEnd(() -> io.setVoltage(CLIMB_RUN_VOLTS), () -> io.setVoltage(Volts.zero()));
   }
 }
