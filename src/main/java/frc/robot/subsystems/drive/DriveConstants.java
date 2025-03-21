d64cc44150d10a2711c76a7a61770b5e072024eb
6672DriveLaptop1 <rishabhreng@gmail.com>
2025-03-21 15:54:18 -0500

    Update at 'Fri Mar 21 15:54:17 CDT 2025'

--- src/main/java/frc/robot/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/drive/DriveConstants.java
@@ -313,14 +313,14 @@ public class DriveConstants {
   // more negative is CLOSER to the reef
   // more positive is FURTHER AWAY from the reef
   public static LoggedTunableNumber autoAlignOutwardLeft =
-      new LoggedTunableNumber("AutoAlign/outLeft", 0.36);
+      new LoggedTunableNumber("AutoAlign/outLeft", 0.46);
   public static LoggedTunableNumber autoAlignOutwardRight =
-      new LoggedTunableNumber("AutoAlign/outRight", 0.36);
+      new LoggedTunableNumber("AutoAlign/outRight", 0.46);
 
   // more negative is to the left OF THE ROBOT
   // more positive is to the right OF THE ROBOT
   public static LoggedTunableNumber autoAlignSidewaysLeft =
       new LoggedTunableNumber("AutoAlign/sideLeft", -0.365);
   public static LoggedTunableNumber autoAlignSidewaysRight =
-      new LoggedTunableNumber("AutoAlign/sideRight", 0.0);
+      new LoggedTunableNumber("AutoAlign/sideRight", 0.02);
 }
